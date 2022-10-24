7d33512efe4c5281a23c85c61fb0ce4212302b77
Tore Stensaker Tefre <toretefre@users.noreply.github.com>
2022-10-24 15:31:37 +0200

    fix: allow common assets files to be used if org file is not provided (#78)
    * allow common assets files to be used if missing org file
    
    * de-indent console statement
    
    * replace console warn with log
    
    * extract helper function exists
    
    * improve function readability

--- packages/assets/src/tools/validate-files.ts
+++ packages/assets/src/tools/validate-files.ts
@@ -1,4 +1,5 @@
 import {ThemeVariant} from '@atb-as/theme';
+import {constants, promises} from 'fs';
 import path from 'path';
 import {vaildOrgIds} from '../generate';
 import {fgNormalizedForUnix, themeVariantAsString} from '../utils';
@@ -18,6 +19,33 @@ async function fromOrgFiles(org: ThemeVariant) {
   return {org, files: files.map(cleanFilenames)};
 }
 
+async function exists(fileToCheck: string) {
+  return await promises
+    .access(fileToCheck, constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
+}
+
+async function verifyThatMissingOrgFilesHasCommonReplacements(
+  missingOrgFiles: string[],
+) {
+  let filesMissingFromBothOrgAndCommonFolder = <string[]>[];
+  for (const missingOrgFile of missingOrgFiles) {
+    const fullPath = path.join(
+      __dirname,
+      '..',
+      '..',
+      'files',
+      'common',
+      missingOrgFile,
+    );
+    const missingOrgFileExistsInCommon = await exists(fullPath);
+    if (!missingOrgFileExistsInCommon)
+      filesMissingFromBothOrgAndCommonFolder.push(fullPath);
+  }
+  return filesMissingFromBothOrgAndCommonFolder;
+}
+
 async function validateValidOrgs() {
   const expectedFiles = await fromOrgFiles(ThemeVariant.AtB);
   const extraOrgs = await Promise.all(
@@ -27,23 +55,34 @@ async function validateValidOrgs() {
   let hasErrors = false;
 
   for (let orgAssets of extraOrgs) {
-    const diff = difference(expectedFiles.files, orgAssets.files);
+    const filesMissingInOrg = difference(expectedFiles.files, orgAssets.files);
+    if (!filesMissingInOrg.length) continue;
 
-    if (diff.length > 0) {
+    console.log(
+      `${themeVariantAsString(
+        orgAssets.org,
+      )} seems to be missing some assets, checking if common files can be used for these files:\n${filesMissingInOrg.join(
+        '\n',
+      )}`,
+    );
+    const filesMissingBothInOrgAndCommon =
+      await verifyThatMissingOrgFilesHasCommonReplacements(filesMissingInOrg);
+
+    if (filesMissingBothInOrgAndCommon.length) {
       hasErrors = true;
       console.error(
-        `
-${themeVariantAsString(orgAssets.org)} is missing some assets:
-${diff.join('\n')}
-
-`,
+        `${themeVariantAsString(
+          orgAssets.org,
+        )} is missing some assets, that also does not have a common file:\n${filesMissingBothInOrgAndCommon.join(
+          '\n',
+        )}`,
       );
     }
   }
 
-  if (hasErrors) {
-    process.exit(1);
-  }
+  hasErrors
+    ? process.exit(1)
+    : console.log('Asset validation completed successfully');
 }
 
 function cleanFilenames(filename: string) {
